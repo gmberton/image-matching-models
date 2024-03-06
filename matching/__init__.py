d5088dde4c97b9a0034c9bd86bb48077672bce08
ga1i13o <48190199+ga1i13o@users.noreply.github.com>
2024-03-06 13:44:14 +0100

    uncomment toolbox methods

--- matching/__init__.py
+++ matching/__init__.py
@@ -1,4 +1,4 @@
-from matching import lightglue, loftr, roma, dedode, handcrafted#, matching_toolbox
+from matching import lightglue, loftr, roma, dedode, handcrafted, matching_toolbox
 
 
 matchers = {
@@ -12,11 +12,11 @@ matchers = {
     'dedode': dedode.DedodeMatcher,
     'sift-nn': handcrafted.SiftNNMatcher,
     'orb-nn': handcrafted.OrbNNMatcher,
-    # 'patch2pix': matching_toolbox.Patch2pixMatcher,
-    # 'patch2pix_superglue': matching_toolbox.SuperGluePatch2pixMatcher,
-    # 'superglue': matching_toolbox.SuperGlueMatcher,
-    # 'r2d2': matching_toolbox.R2D2Matcher,
-    # 'd2net': matching_toolbox.D2netMatcher
+    'patch2pix': matching_toolbox.Patch2pixMatcher,
+    'patch2pix_superglue': matching_toolbox.SuperGluePatch2pixMatcher,
+    'superglue': matching_toolbox.SuperGlueMatcher,
+    'r2d2': matching_toolbox.R2D2Matcher,
+    'd2net': matching_toolbox.D2netMatcher
 }
 
 
