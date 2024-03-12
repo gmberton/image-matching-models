900350a985e24f34e15bcca0756c43f15c063831
Alex Stoken <astoken@email.arizona.edu>
2024-03-12 15:02:31 -0500

    se2loftr tested and working; add se2loftr params in run_matching

--- matching/se2loftr.py
+++ matching/se2loftr.py
@@ -4,40 +4,60 @@ from pathlib import Path
 import torchvision.transforms as tfm
 import torch
 import os
+import urllib.request
+import gdown
+
 
 sys.path.append(str(Path('third_party/Se2_LoFTR')))
 from src.loftr.loftr import LoFTR
-from configs.loftr.outoor.loftr_ds_e2_dense_8rot import cfg as rot8_cfg
-from configs.loftr.outoor.loftr_ds_e2_dense_big import cfg as big_cfg
-from configs.loftr.outoor.loftr_ds_e2_dense import cfg as e2dense_cfg
-from configs.loftr.outoor.loftr_ds_e2 import cfg as baseline_cfg
+from configs.loftr.outdoor.loftr_ds_e2_dense_8rot import cfg as rot8_cfg
+from configs.loftr.outdoor.loftr_ds_e2_dense_big import cfg as big_cfg
+from configs.loftr.outdoor.loftr_ds_e2_dense import cfg as e2dense_cfg
+from configs.loftr.outdoor.loftr_ds_e2 import cfg as baseline_cfg
 
 from src.utils.misc import lower_config
 
 from matching.base_matcher import BaseMatcher
 
 class Se2LoFTRMatcher(BaseMatcher):
+    #TODO 
+    # dense and base loftr have shape mismatches in state dict load
     configs = {'rot8': rot8_cfg,
                'big': big_cfg,
                'dense':e2dense_cfg,
-               'loftr': baseline_cfg}
+            #    'loftr': baseline_cfg
+               }
 
-    weights = {{'rot8': '8rot_ckpt.ckpt',
+    weights = {'rot8': '8rot.ckpt',
                'big': '4rot_big.ckpt',
-               'dense':'4rot.ckpt',
-               'loftr': 'baseline.ckpt'}}
+            #    'dense':'4rot.ckpt',
+            #    'loftr': 'baseline.ckpt'
+               }
 
-    def __init__(self, device='cpu', config='rot8') -> None:
+    weights_url = {'rot8': 'https://drive.google.com/file/d/1jPtOTxmwo1Z_YYP2YMS6efOevDaNiJR4/view',
+               'big': 'https://drive.google.com/file/d/1AE_EmmhQLfArIP-zokSlleY2YiSgBV3m/view',
+            #    'dense':'https://drive.google.com/file/d/17vxdnVtjVuq2m8qJsOG1JFfJjAqcgr4j/view',
+            #    'loftr': 'https://drive.google.com/file/d/1OylPSrbjzRJgvLHM3qJPAVpW3BEQeuFS/view'
+               }
+
+    def __init__(self, device='cpu', max_num_keypoints=0, loftr_config='rot8', *args, **kwargs) -> None:
         super().__init__(device)
-
         os.makedirs("model_weights", exist_ok=True)
-        weights_path = Path('model_weights').joinpaht(weights[config])
+        self.weights_path = Path('model_weights').joinpath(Se2LoFTRMatcher.weights[loftr_config])
         if not os.path.isfile(self.weights_path):
+            print(f"Downloading {Se2LoFTRMatcher.weights_url[loftr_config]}")
+            gdown.download(Se2LoFTRMatcher.weights_url[loftr_config],
+                                        output=str(self.weights_path),
+                                        fuzzy=True)
+    
 
+        self.model = self.load_model(loftr_config, device)
 
-        self.model = LoFTR(config=lower_config(configs[config])).to(self.device)
-        self.model.load_state_dict(torch.load(weights_path), map_location=device)['state_dict'])
-        self.model = self.model.eval()
+    def load_model(self, config, device='cpu'):
+        model = LoFTR(config=lower_config(Se2LoFTRMatcher.configs[config])['loftr']).to(self.device)
+        model.load_state_dict({k.replace('matcher.', ''): v for k, v in torch.load(self.weights_path, map_location=device)['state_dict'].items()})
+
+        return model.eval()
 
     def forward(self, img0, img1):
         super().forward(img0, img1)
@@ -47,8 +67,10 @@ class Se2LoFTRMatcher(BaseMatcher):
         img1 = tfm.Grayscale()(img1).unsqueeze(0).to(self.device)
 
         batch = {'image0': img0, 'image1': img1}
-        output = self.model(batch)
-        mkpts0, mkpts1 = output["keypoints0"], output["keypoints1"]
+        self.model(batch) # loftr does not return anything, instead stores results in batch dict
+        # batch now has keys: ['mkpts0_f', 'mkpts1_f', 'expec_f','mkpts0_c', 'mkpts1_c', 'mconf', 'm_bids','gt_mask']
+
+        mkpts0, mkpts1 = batch["mkpts0_f"], batch["mkpts1_f"]
 
         # process_matches is implemented by the parent BaseMatcher, it is the
         # same for all methods, given the matched keypoints
--- run_matching.py
+++ run_matching.py
@@ -25,7 +25,7 @@ def main(args):
 
     # Choose a matcher
     matcher = get_matcher(args.matcher, device=args.device, max_num_keypoints=args.n_kpts,
-        dedode_thresh=args.dedode_thresh, lowe_thresh=args.lowe_thresh
+        dedode_thresh=args.dedode_thresh, lowe_thresh=args.lowe_thresh, loftr_config=args.loftr_config
     )
 
     pair_folders = sorted(glob('assets/pair*'))
@@ -58,11 +58,13 @@ if __name__ == '__main__':
     # shared by all methods:
     parser.add_argument('--im_size', type=int, default=512, help='resize im to im_size x im_size')
     parser.add_argument('--n_kpts', type=int, default=2048, help='max num keypoints')
+    parser.add_argument('--device', type=str, default='cpu', choices=["cpu", "cuda"])
+
     # method-specific
     parser.add_argument('--dedode_thresh', type=float, default=0.05, help='threshold on match confidence for DeDoDe')
     parser.add_argument('--lowe_thresh', type=float, default=0.75, help='threshold on lowe ratio test for SIFT or ORB')
 
-    parser.add_argument('--device', type=str, default='cpu', choices=["cpu", "cuda"])
-
+    # se2loftr
+    parser.add_argument('--loftr_config', type=str, default='rot8', help='loftr config to use, choose from [rot8, big]')
     args = parser.parse_args()
     main(args)
