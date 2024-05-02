584fef97f4e68f33d32406c8225fac3142c5e0ba
Gabriele Berton <s252805@studenti.polito.it>
2024-05-02 18:24:41 -0400

    Cleaning run_matching.py

--- run_matching.py
+++ run_matching.py
@@ -1,12 +1,8 @@
 
-import os
 import sys
-import torch
 import argparse
 import matplotlib
-from glob import glob
 from pathlib import Path
-from os.path import join
 
 sys.path.append(str(Path('third_party/LightGlue')))
 from lightglue import viz2d
@@ -18,35 +14,31 @@ if not hasattr(sys, 'ps1'):
     matplotlib.use('Agg')
 
 def main(args):
+    assets_dir = Path("assets")
+    
     image_size = [args.im_size, args.im_size]
-    out_dir = join('assets', f'out_{args.matcher}')
-    os.makedirs(out_dir, exist_ok=True)
+    args.out_dir.mkdir(exist_ok=True, parents=True)
 
     # Choose a matcher
     matcher = get_matcher(args.matcher, device=args.device, max_num_keypoints=args.n_kpts,
         dedode_thresh=args.dedode_thresh, lowe_thresh=args.lowe_thresh, loftr_config=args.loftr_config
     )
 
-    pair_folders = sorted(glob('assets/pair*'))
-    img_pairs = []
-    for pair_folder in pair_folders:
-        query = glob(join(pair_folder, '*.jpg'))[0]
-        pred = glob(join(pair_folder, '*.png'))[0]
-        img_pairs.append((query, pred))
-
-    for i, img_pair in enumerate(img_pairs):
-        print(f'\n[---]Folder: {pair_folders[i]}: [---]')
-        p1, p2 = img_pair
-
-        image0 = matcher.image_loader(p1, resize=image_size).to(args.device)
-        image1 = matcher.image_loader(p2, resize=image_size).to(args.device)
+    pair_dirs = sorted(assets_dir.glob("pair*"))
+    for i, pair_dir in enumerate(pair_dirs):
+        img0_path, img1_path = list(pair_dir.glob('*'))
+        
+        image0 = matcher.image_loader(img0_path, resize=image_size).to(args.device)
+        image1 = matcher.image_loader(img1_path, resize=image_size).to(args.device)
         score, fm, mkpts0, mkpts1 = matcher(image0, image1)
-        print(f"Found n. inliers after RANSAC: {score} ")
 
         axes = viz2d.plot_images([image0, image1])
         viz2d.plot_matches(mkpts0, mkpts1, color="lime", lw=0.2)
         viz2d.add_text(0, f'{len(mkpts1)} matches', fs=20)
-        viz2d.save_plot(join(out_dir, f"output_{i}.jpg"))
+        viz_path = (args.out_dir / f"output_{i}.jpg")
+        viz2d.save_plot(viz_path)
+        
+        print(f"Folder: {pair_dir}. Found {score} inliers after RANSAC. Viz saved in {viz_path}")
 
 
 if __name__ == '__main__':
@@ -58,6 +50,8 @@ if __name__ == '__main__':
     parser.add_argument('--im_size', type=int, default=512, help='resize im to im_size x im_size')
     parser.add_argument('--n_kpts', type=int, default=2048, help='max num keypoints')
     parser.add_argument('--device', type=str, default='cpu', choices=["cpu", "cuda"])
+    
+    parser.add_argument('--out_dir', type=str, default=None, help='path where outputs are saved')
 
     # method-specific
     parser.add_argument('--dedode_thresh', type=float, default=0.05, help='threshold on match confidence for DeDoDe')
@@ -70,4 +64,9 @@ if __name__ == '__main__':
     parser.add_argument('--steerer_type', type=str, default='C8', help='Steerer type, choose from [C8, C4, SO2]')
     
     args = parser.parse_args()
+    
+    if args.out_dir is None:
+        args.out_dir = args.matcher
+    args.out_dir = Path("outputs") / args.out_dir
+    
     main(args)
