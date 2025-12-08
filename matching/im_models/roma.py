a5f7cbb0c76fbccaab3a70f962006eb2494bcb85
Alex Stoken <astoken@email.arizona.edu>
2025-12-07 22:01:07 -0700

    working romav2

--- matching/im_models/roma.py
+++ matching/im_models/roma.py
@@ -5,9 +5,10 @@ from kornia.utils import tensor_to_image
 import tempfile
 from pathlib import Path
 
-
 from matching import BaseMatcher, THIRD_PARTY_DIR
-from matching.utils import add_to_path, install_package
+from matching.utils import add_to_path
+
+import torchvision.transforms as tfm
 
 add_to_path(THIRD_PARTY_DIR.joinpath("RoMa"))
 from romatch import roma_outdoor, tiny_roma_v1_outdoor
@@ -16,9 +17,7 @@ from romatch import roma_outdoor, tiny_roma_v1_outdoor
 from PIL import Image
 from skimage.util import img_as_ubyte
 
-
-import subprocess
-import sys
+from romav2 import RoMaV2
 
 
 class RomaMatcher(BaseMatcher):
@@ -29,9 +28,7 @@ class RomaMatcher(BaseMatcher):
         super().__init__(device, **kwargs)
         self.roma_model = roma_outdoor(device=device)
         self.max_keypoints = max_num_keypoints
-        self.normalize = tfm.Normalize(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        )
+        self.normalize = tfm.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         self.roma_model.train(False)
 
     def compute_padding(self, img0, img1):
@@ -60,17 +57,13 @@ class RomaMatcher(BaseMatcher):
         w0, h0 = img0_size
         w1, h1 = img1_size
 
-        warp, certainty = self.roma_model.match(
-            img0_temp.name, img1_temp.name, batched=False, device=self.device
-        )
+        warp, certainty = self.roma_model.match(img0_temp.name, img1_temp.name, batched=False, device=self.device)
 
         img0_temp.close(), img1_temp.close()
         Path(img0_temp.name).unlink()
         Path(img1_temp.name).unlink()
 
-        matches, certainty = self.roma_model.sample(
-            warp, certainty, num=self.max_keypoints
-        )
+        matches, certainty = self.roma_model.sample(warp, certainty, num=self.max_keypoints)
         mkpts0, mkpts1 = self.roma_model.to_pixel_coordinates(matches, h0, w0, h1, w1)
 
         return mkpts0, mkpts1, None, None, None, None
@@ -82,9 +75,7 @@ class TinyRomaMatcher(BaseMatcher):
         super().__init__(device, **kwargs)
         self.roma_model = tiny_roma_v1_outdoor(device=device)
         self.max_keypoints = max_num_keypoints
-        self.normalize = tfm.Normalize(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        )
+        self.normalize = tfm.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         self.roma_model.train(False)
 
     def preprocess(self, img):
@@ -100,9 +91,7 @@ class TinyRomaMatcher(BaseMatcher):
         # batch = {"im_A": img0.to(self.device), "im_B": img1.to(self.device)}
         warp, certainty = self.roma_model.match(img0, img1, batched=False)
 
-        matches, certainty = self.roma_model.sample(
-            warp, certainty, num=self.max_keypoints
-        )
+        matches, certainty = self.roma_model.sample(warp, certainty, num=self.max_keypoints)
         mkpts0, mkpts1 = self.roma_model.to_pixel_coordinates(matches, h0, w0, h1, w1)
 
         return mkpts0, mkpts1, None, None, None, None
@@ -110,15 +99,15 @@ class TinyRomaMatcher(BaseMatcher):
 
 class Romav2Matcher(BaseMatcher):
     def __init__(self, device="cpu", max_num_keypoints=2048, *args, **kwargs):
-        try:
-            from romav2 import RoMaV2
-        except ModuleNotFoundError as e:
-            print("romav2 not found, installing...")
-            install_package(str(THIRD_PARTY_DIR.joinpath("RoMaV2")), editable=True)
-            from romav2 import RoMaV2
+        # try:
+        #     from romav2 import RoMaV2
+        # except ModuleNotFoundError as e:
+        #     print("romav2 not found, installing...")
+        #     install_package(str(THIRD_PARTY_DIR.joinpath("RoMaV2")), editable=True)
+        #     from romav2 import RoMaV2
 
         super().__init__(device, **kwargs)
-        self.model = RoMaV2(device=device)
+        self.model = RoMaV2().to(self.device)
         self.max_keypoints = max_num_keypoints
 
     def compute_padding(self, img0, img1):
@@ -129,34 +118,35 @@ class Romav2Matcher(BaseMatcher):
         self.pad = PadTo((pad_dim, pad_dim), keepdim=True)
 
     def preprocess(self, img: torch.Tensor, pad=False) -> Image:
-        if isinstance(img, torch.Tensor) and img.dtype == (torch.float):
-            img = torch.clamp(img, -1, 1)
-        if pad:
-            img = self.pad(img)
-        img = tensor_to_image(img)
-        pil_img = Image.fromarray(img_as_ubyte(img), mode="RGB")
-        temp = tempfile.NamedTemporaryFile("w+b", suffix=".png", delete=False)
-        pil_img.save(temp.name, format="png")
-        return temp, pil_img.size
+        # H, W = (
+        #     (self.model.H_lr, self.model.W_lr)
+        #     if (self.model.H_hr is None or self.model.W_hr is None)
+        #     else (self.model.H_hr, self.model.W_hr)
+        # )
+        # _, h, w = img.shape
+        # orig_shape = h, w
+
+        # img = tfm.Resize((H, W), antialias=True)(img)
+
+        return img.unsqueeze(0).to(self.device), orig_shape
 
     def _forward(self, img0, img1, pad=False):
         if pad:
             self.compute_padding(img0, img1)
-        img0_temp, img0_size = self.preprocess(img0)
-        img1_temp, img1_size = self.preprocess(img1)
+        img0, img0_size = self.preprocess(img0)
+        print(img0)
+        img1, img1_size = self.preprocess(img1)
         w0, h0 = img0_size
         w1, h1 = img1_size
 
         # Match densely for any image-like pair of inputs
-        preds = self.model.match(img0_temp, img1_temp)
+        preds = self.model.match(img0, img1)
 
-        img0_temp.close(), img1_temp.close()
-        Path(img0_temp.name).unlink()
-        Path(img1_temp.name).unlink()
+        # img0_temp.close(), img1_temp.close()
+        # Path(img0_temp.name).unlink()
+        # Path(img1_temp.name).unlink()
 
-        matches, overlaps, precision_AB, precision_BA = self.model.sample(
-            preds, self.max_keypoints
-        )
+        matches, overlaps, precision_AB, precision_BA = self.model.sample(preds, self.max_keypoints)
 
         # Convert to pixel coordinates (RoMaV2 produces matches in [-1,1]x[-1,1])
         mkpts0, mkpts1 = self.model.to_pixel_coordinates(matches, h0, w0, h1, w1)
