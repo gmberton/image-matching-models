74e1d194ac706b8937ac0fd298c1bd3bd5a71294
gmberton <s252805@studenti.polito.it>
2026-01-09 04:09:19 +0100

    Renamed result_dict -> result for consistency

--- matching/viz.py
+++ matching/viz.py
@@ -14,7 +14,7 @@ if not hasattr(sys, "ps1"):
 def plot_matches(
     img0: np.ndarray,
     img1: np.ndarray,
-    result_dict: dict,
+    result: dict,
     show_matched_kpts=True,
     show_all_kpts=False,
     save_path=None,
@@ -24,7 +24,7 @@ def plot_matches(
     Args:
         img0 (np.ndarray): img0 from matching procedure
         img1 (np.ndarray): img1 from matching procedure
-        result_dict (dict): result from BaseMatcher, must contain keys: ['inlier_kpts0', 'inlier_kpts1', 'matched_kpts0', 'matched_kpts1']
+        result (dict): result from BaseMatcher, must contain keys: ['inlier_kpts0', 'inlier_kpts1', 'matched_kpts0', 'matched_kpts1']
         show_matched_kpts (bool, optional): Show matched kpts in addition to inliers. Matched kpts are blue. Defaults to True.
         show_all_kpts (bool, optional): Show all detected kpts in red. Defaults to False.
         save_path (str| Path, optional): path to save file to. Not saved if None. Defaults to None.
@@ -34,26 +34,26 @@ def plot_matches(
     """
     ax = viz2d.plot_images([img0, img1])
 
-    if show_matched_kpts and "matched_kpts0" in result_dict.keys():
+    if show_matched_kpts and "matched_kpts0" in result.keys():
         viz2d.plot_matches(
-            result_dict["matched_kpts0"],
-            result_dict["matched_kpts1"],
+            result["matched_kpts0"],
+            result["matched_kpts1"],
             color="blue",
             lw=0.05,
             ps=2,
         )
 
-    if show_all_kpts and result_dict["all_kpts0"] is not None:
-        viz2d.plot_keypoints([result_dict["all_kpts0"], result_dict["all_kpts1"]], colors="red", ps=2)
+    if show_all_kpts and result["all_kpts0"] is not None:
+        viz2d.plot_keypoints([result["all_kpts0"], result["all_kpts1"]], colors="red", ps=2)
 
-    viz2d.plot_matches(result_dict["inlier_kpts0"], result_dict["inlier_kpts1"], color="lime", lw=0.2)
-    if len(result_dict["matched_kpts1"]):
-        ratio = f"{len(result_dict['inlier_kpts0']) / len(result_dict['matched_kpts1']):0.2f}"
+    viz2d.plot_matches(result["inlier_kpts0"], result["inlier_kpts1"], color="lime", lw=0.2)
+    if len(result["matched_kpts1"]):
+        ratio = f"{len(result['inlier_kpts0']) / len(result['matched_kpts1']):0.2f}"
     else:
         ratio = "N/A"
     viz2d.add_text(
         0,
-        f"{len(result_dict['inlier_kpts0'])} inliers/{len(result_dict['matched_kpts1'])} matches\n({ratio}) inlier ratio)",
+        f"{len(result['inlier_kpts0'])} inliers/{len(result['matched_kpts1'])} matches\n({ratio}) inlier ratio)",
         fs=17,
         lwidth=2,
     )
@@ -67,12 +67,12 @@ def plot_matches(
     return ax
 
 
-def plot_kpts(img0, result_dict, model_name="", save_path=None):
+def plot_kpts(img0, result, model_name="", save_path=None):
     """Plot keypoints in one image.
 
     Args:
         img0 (np.ndarray): img keypoints are detected from.
-        result_dict (dict): return from BaseMatcher. Must contain ['all_kpts0']
+        result (dict): return from BaseMatcher. Must contain ['all_kpts0']
         save_path (str| Path, optional): path to save file to. Not saved if None. Defaults to None.
 
     Returns:
@@ -81,10 +81,10 @@ def plot_kpts(img0, result_dict, model_name="", save_path=None):
     if len(model_name):
         model_name = " - " + model_name
     ax = viz2d.plot_images([img0])
-    viz2d.plot_keypoints([result_dict["all_kpts0"]], colors="orange", ps=10)
-    viz2d.add_text(0, f"{len(result_dict['all_kpts0'])} kpts" + model_name, fs=20)
+    viz2d.plot_keypoints([result["all_kpts0"]], colors="orange", ps=10)
+    viz2d.add_text(0, f"{len(result['all_kpts0'])} kpts" + model_name, fs=20)
     if model_name is not None:
-        viz2d.add_text(0, f"{len(result_dict['all_kpts0'])}", fs=20)
+        viz2d.add_text(0, f"{len(result['all_kpts0'])}", fs=20)
 
     if save_path is not None:
         viz2d.save_plot(save_path)
@@ -110,13 +110,13 @@ def add_alpha_channel(img: np.ndarray) -> np.ndarray:
         return img
 
 
-def stitch(img0: np.ndarray | torch.Tensor, img1: np.ndarray | torch.Tensor, result_dict: dict) -> np.ndarray:
+def stitch(img0: np.ndarray | torch.Tensor, img1: np.ndarray | torch.Tensor, result: dict) -> np.ndarray:
     """Stich two images together.
 
     Args:
         img0 (np.ndarray | torch.Tensor): img0 (to be warped as part of stitching)
         img1 (np.ndarray | torch.Tensor): img1 (is not warped in stitching)
-        result_dict (dict): BaseMatcher return dict of results. Required keys: ["H"]
+        result (dict): BaseMatcher return dict of results. Required keys: ["H"]
 
     Returns:
         np.ndarray: stitched images as array
@@ -137,7 +137,7 @@ def stitch(img0: np.ndarray | torch.Tensor, img1: np.ndarray | torch.Tensor, res
     corners1 = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
 
     # Warp the corners of the first image using the homography
-    warped_corners0 = cv2.perspectiveTransform(corners0, result_dict["H"])
+    warped_corners0 = cv2.perspectiveTransform(corners0, result["H"])
 
     # Combine all corners and find min/max to find the overall bounding dimensions
     all_corners = np.concatenate((warped_corners0, corners1), axis=0)
@@ -149,7 +149,7 @@ def stitch(img0: np.ndarray | torch.Tensor, img1: np.ndarray | torch.Tensor, res
     H_translation = np.array([[1, 0, translation[0]], [0, 1, translation[1]], [0, 0, 1]])
 
     # warp first image onto stitch
-    stitched_imgs = cv2.warpPerspective(img0, H_translation.dot(result_dict["H"]), (x_max - x_min, y_max - y_min))
+    stitched_imgs = cv2.warpPerspective(img0, H_translation.dot(result["H"]), (x_max - x_min, y_max - y_min))
 
     # overlay second image onto stitch
     stitched_imgs[translation[1] : translation[1] + h1, translation[0] : translation[0] + w1] = img1
