b33ba369bf4e37f83d93b428fbb64a3823024be9
Alex Stoken <astoken@email.arizona.edu>
2026-04-19 22:20:25 -0700

    add clidd matcher [WIP]

--- vismatch/__init__.py
+++ vismatch/__init__.py
@@ -97,6 +97,7 @@ available_models = [
     "xfeat-star-steerers-learned",
     "loma",
     "loma-r",
+    "clidd",
 ]
 
 
@@ -488,6 +489,10 @@ def get_matcher(
         from vismatch.im_models import loma
 
         return loma.LoMaMatcher(device, max_num_keypoints, arch="LoMa-R", *args, **kwargs)
+    elif matcher_name == "clidd":
+        from vismatch.im_models import clidd
+
+        return clidd.CLIDDMatcher(device, arch="U128", *args, **kwargs)
 
     else:
         raise RuntimeError(
