7b5b18731bf4b1172340b7df1ed51160bc9a47dd
Gabriele Berton <49533159+gmberton@users.noreply.github.com>
2026-07-06 06:09:53 +0200

    Host aspanformer weights on HuggingFace

--- vismatch/im_models/aspanformer.py
+++ vismatch/im_models/aspanformer.py
@@ -1,8 +1,6 @@
 import torch
 from pathlib import Path
-import gdown
 import torchvision.transforms as tfm
-import tarfile
 from huggingface_hub import snapshot_download
 
 from vismatch import THIRD_PARTY_DIR, BaseMatcher
@@ -17,15 +15,11 @@ from src.config.default import get_cfg_defaults as aspan_cfg_defaults
 
 class AspanformerMatcher(BaseMatcher):
     hf_model_id = "vismatch/aspanformer"
-    weights_src = "https://drive.google.com/file/d/1eavM9dTkw9nbc-JqlVVfGPU5UvTTfc6k/view"
     divisible_size = 32
 
     def __init__(self, device="cpu", **kwargs):
         super().__init__(device, **kwargs)
-        cache_dir = Path(snapshot_download(self.hf_model_id))
-        self.weights_path = cache_dir / "weights" / "outdoor.ckpt"
-
-        self.download_weights(cache_dir)
+        self.weights_path = Path(snapshot_download(self.hf_model_id)) / "outdoor.ckpt"
 
         config = aspan_cfg_defaults()
         config.merge_from_file(BASE_PATH.joinpath("configs", "aspan", "outdoor", "aspan_test.py"))
@@ -36,17 +30,6 @@ class AspanformerMatcher(BaseMatcher):
         )
 
         self.matcher = self.matcher.to(device).eval()
-
-    def download_weights(self, cache_dir):
-        if not self.weights_path.is_file():
-            print("Downloading Aspanformer outdoor... (takes a while)")
-            gdown.download(
-                self.weights_src,
-                output=str(cache_dir / "weights_aspanformer.tar"),
-            )
-            tar = tarfile.open(cache_dir / "weights_aspanformer.tar")
-            tar.extractall(cache_dir)
-            tar.close()
 
     def preprocess(self, img):
         _, h, w = img.shape
